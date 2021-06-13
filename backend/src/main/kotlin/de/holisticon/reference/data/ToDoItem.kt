70bc838956d639cc4449b365630138e74d79826b
Lukas Taake <lukas.taake@holisticon.de>
2021-06-13 22:18:32 +0200

    feat(backend): implement controller for user api

--- backend/src/main/kotlin/de/holisticon/reference/data/ToDoItem.kt
+++ backend/src/main/kotlin/de/holisticon/reference/data/ToDoItem.kt
@@ -8,7 +8,7 @@ import javax.persistence.Id
 import javax.persistence.Table
 
 @Entity
-@Table(name = "user")
+@Table(name = "todo")
 data class ToDoItem(
     @Id
     var id: Long? = null,
--- backend/src/main/kotlin/de/holisticon/reference/data/UserController.kt
+++ backend/src/main/kotlin/de/holisticon/reference/data/UserController.kt
@@ -1,6 +1,10 @@
 package de.holisticon.reference.data
 
 import de.holisticon.reference.rest.api.UserApi
+import de.holisticon.reference.rest.model.UserDto
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -9,4 +13,19 @@ class UserController(
     private val userConverter: UserConverter
 ): UserApi {
 
+  override fun createUser(@PathVariable userId: String, @RequestBody user: UserDto): ResponseEntity<UserDto> {
+    val entity = userConverter.toEntity(user)
+    val result = userRepository.save(entity)
+    val response = userConverter.toDto(result)
+    return ResponseEntity.ok(response)
+  }
+
+  override fun getUser(@PathVariable userId: String): ResponseEntity<UserDto> {
+    val entity = userRepository.findById(userId.toLong())
+    if (entity.isEmpty) {
+      return ResponseEntity.notFound().build()
+    }
+    val response = userConverter.toDto(entity.get())
+    return ResponseEntity.ok(response)
+  }
 }
