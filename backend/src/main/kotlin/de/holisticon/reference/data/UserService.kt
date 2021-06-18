98f1aea7a19de041cf5af9676e1e62947425a4b6
Lukas Taake <lukas.taake@holisticon.de>
2021-06-18 22:53:49 +0200

    feat(backend): Implement actual data encryption

--- backend/src/main/kotlin/de/holisticon/reference/data/UserService.kt
+++ backend/src/main/kotlin/de/holisticon/reference/data/UserService.kt
@@ -1,18 +1,33 @@
 package de.holisticon.reference.data
 
+import de.holisticon.reference.crypto.CryptoService
 import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
 class UserService(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val cryptoService: CryptoService
 ) {
 
   fun create(user: User): User {
-    return userRepository.save(user)
+    val encryptedUser = encrypt(user)
+    val savedUser = userRepository.save(encryptedUser)
+    return decrypt(savedUser)
   }
 
   fun find(userId: Long): Optional<User> {
-    return userRepository.findById(userId)
+    val encryptedUser = userRepository.findById(userId)
+    return encryptedUser.map(this::decrypt)
+  }
+
+  private fun encrypt(user: User): User {
+    val encryptedName = cryptoService.encrypt(user.id.toString(), user.name)
+    return User(user.id, encryptedName, user.toDoItems)
+  }
+
+  private fun decrypt(user: User): User {
+    val decryptedName = cryptoService.decrypt(user.id.toString(), user.name)
+    return User(user.id, decryptedName, user.toDoItems)
   }
 }
