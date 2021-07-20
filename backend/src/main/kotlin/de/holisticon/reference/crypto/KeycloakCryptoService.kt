ebf10d145c323b2a3713544de0c5e8affd9005bf
Martin Reinhardt <contact@martinreinhardt-online.de>
2021-07-20 12:16:28 +0200

    feat(Security): Use Keycloak REST Template for reusing auth from client

--- backend/src/main/kotlin/de/holisticon/reference/crypto/KeycloakCryptoService.kt
+++ backend/src/main/kotlin/de/holisticon/reference/crypto/KeycloakCryptoService.kt
@@ -1,25 +1,27 @@
 package de.holisticon.reference.crypto
 
+import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 
 @Service
-class KeycloakCryptoService : CryptoService {
+class KeycloakCryptoService(
+  private val restTemplate: KeycloakRestTemplate
+) : CryptoService {
 
   companion object {
-    private const val KEYCLOAK_URL = "http://localhost:9090/auth/realms/master"
+    // TODO use from config
+    private const val KEYCLOAK_URL = "http://localhost:9090/auth/realms/app"
     const val ENCRYPT_ENDPOINT = "$KEYCLOAK_URL/gdpr/encrypt/"
     const val DECRYPT_ENDPOINT = "$KEYCLOAK_URL/gdpr/decrypt/"
   }
 
   override fun encrypt(userId: String, data: String): String {
-    val restTemplate = RestTemplate()
     val encryptedData = restTemplate.postForObject(ENCRYPT_ENDPOINT, DecryptedData(userId, data), EncryptedData::class.java)
     return encryptedData.cipherText
   }
 
   override fun decrypt(userId: String, cipherText: String): String {
-    val restTemplate = RestTemplate()
     val encryptedData = restTemplate.postForObject(DECRYPT_ENDPOINT, EncryptedData(userId, cipherText), DecryptedData::class.java)
     return encryptedData.data
   }
